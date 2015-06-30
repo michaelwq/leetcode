4dbe2acef844329be896e0989e020564d7de82a1
michael wang <michaelwqwq@gmail.com>
2015-06-30 19:21:50 -0400

    Git first Project Test

---
+++ LeetCode/src/test/java/com/leetcode/hello/Hello.java
@@ -0,0 +1,19 @@
+
+/**
+ * @author michaelwq
+ *
+ */
+package com.leetcode.hello;
+
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git Init Hello!");
+
+	}
+
+}
