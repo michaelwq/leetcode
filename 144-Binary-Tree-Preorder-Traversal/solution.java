3250e8714f26344c85fe0b839a3190fee2f06038
michael wang <michaelwqwq@gmail.com>
2016-05-17 15:51:44 -0400

    [144. Binary Tree Preorder Traversal][Accepted]committed by LeetCode Extension

--- 144-Binary-Tree-Preorder-Traversal/solution.java
+++ 144-Binary-Tree-Preorder-Traversal/solution.java
@@ -8,23 +8,45 @@
  * }
  */
 public class Solution {
-    List<Integer> result=new ArrayList<Integer>();
-    public List<Integer> preorderTraversal(TreeNode root) {
-		test(root);
-		return result;
+//     List<Integer> result=new ArrayList<Integer>();
+//     public List<Integer> preorderTraversal(TreeNode root) {
+// 		test(root);
+// 		return result;
         
-    }
+//     }
 	
-	public void test(TreeNode root){
-		if(root!=null){
-			result.add(root.val);
-			if(root.left!=null){
-			test(root.left);
-    		}
-    		if(root.right!=null){
-    			test(root.right);
-    		}
+// 	public void test(TreeNode root){
+// 		if(root!=null){
+// 			result.add(root.val);
+// 			if(root.left!=null){
+// 			test(root.left);
+//     		}
+//     		if(root.right!=null){
+//     			test(root.right);
+//     		}
+// 		}
+		
+// 	}
+    Stack<TreeNode> stack=new Stack<TreeNode>();
+	List<Integer> result=new ArrayList<Integer>();
+	public List<Integer> preorderTraversal(TreeNode root) {
+		if(root==null){
+			return result;
+		}else{
+			stack.push(root);
 		}
+		while(!stack.empty()){
+			TreeNode treeNode=stack.pop();
+			result.add(treeNode.val);
+			if(treeNode.right!=null){
+				stack.push(treeNode.right);
+			}
+			if(treeNode.left!=null){
+				stack.push(treeNode.left);
+			}	
+		}
+		
+		return result;
 		
 	}
 }
