1fef336dd094a5738bf07cfbdd01de8d6f40abaf
michael wang <michaelwqwq@gmail.com>
2016-08-11 16:30:54 -0400

    [62. Unique Paths][Accepted]committed by LeetCode Extension

--- 62-Unique-Paths/solution.java
+++ 62-Unique-Paths/solution.java
@@ -1,36 +1,19 @@
 public class Solution {
     public int uniquePaths(int m, int n) {
-		ResultSet resultSet = new ResultSet(0, 0, 1);
-        return getPath(resultSet, m, n).result;
-        
+        if ( m == 0 || n == 0){
+            return 0;
+        }
+        int[][] grid = new int[m][n];
+        //grid[0][0] = 1;
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                if ( i == 0 || j == 0){
+                    grid[i][j] = 1;
+                }else {
+                    grid[i][j] = grid[i-1][j] + grid[i][j-1];
+                }
+            }
+        }
+        return grid[m-1][n-1];
     }
-    private ResultSet getPath(ResultSet resultSet, int m_limit, int n_limit){
-        if(resultSet.m < m_limit - 1 && resultSet.n < n_limit - 1){
-			int m = resultSet.m;
-			int n = resultSet.n;
-			int result = resultSet.result;
-			if(resultSet.m < m_limit - 1){
-				ResultSet right = new ResultSet(m+1, n, result);
-				right = getPath(right , m_limit, n_limit);
-				result = right.result;
-			}
-			if (resultSet.n < n_limit - 1){
-				ResultSet down = new ResultSet(m, n+1, result + 1);
-				down = getPath(down , m_limit, n_limit);
-				result = down.result;
-			}
-			return new ResultSet(m_limit, n_limit, result) ;
-		}
-		return resultSet;
-    }
-	class ResultSet{
-		int m;
-		int n;
-		int result;
-		ResultSet(int m, int n, int result){
-			this.m = m;
-			this.n = n;
-			this.result = result;
-		}
-	}
 }
