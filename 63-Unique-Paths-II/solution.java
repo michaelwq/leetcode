9fdfbc04eca8b209af2e130e282d0f6f6da5330a
michael wang <michaelwqwq@gmail.com>
2016-08-11 17:49:22 -0400

    [63. Unique Paths II][Accepted]committed by LeetCode Extension

--- 63-Unique-Paths-II/solution.java
+++ 63-Unique-Paths-II/solution.java
@@ -2,28 +2,38 @@ public class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
-        if (m == 0 || n == 0){
+        if (m == 0 || n == 0 || obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1){
             return 0;
         }
         int[][] dp = new int[m][n];
+        for (int j = 0; j < n; j++){
+            if( obstacleGrid[0][j] != 1){
+                dp[0][j] = 1;
+            }else {
+                break;
+            }
+        }
         for (int i = 0; i < m; i++){
-            for (int j = 0; j < n; j++){
-                if (obstacleGrid[i][j]!= 1){
-                    if (i == 0 || j == 0){
-                        dp[i][j] = 1;
-                    }else {
-                        int tmp = 0;
-                        if (obstacleGrid[i-1][j] != 1){
-                            tmp += dp[i-1][j];
-                        }
-                        if (obstacleGrid[i][j-1] != 1){
-                            tmp += dp[i][j-1];
-                        }
-                        dp[i][j] = tmp;
+            if( obstacleGrid[i][0] != 1){
+                dp[i][0] = 1;
+            }else {
+                break;
+            }
+        }
+        for (int i = 1; i < m; i++){
+            for (int j = 1; j < n; j++){
+                if( obstacleGrid[i][j] != 1){
+                    int tmp = 0;
+                    if (obstacleGrid[i-1][j] != 1){
+                        tmp += dp[i-1][j];
                     }
+                    if (obstacleGrid[i][j-1] != 1){
+                        tmp += dp[i][j-1];
+                    }
+                    dp[i][j] = tmp;
                 }
             }
         }
-        return dp[m][n];
+        return dp[m-1][n-1];
     }
 }
